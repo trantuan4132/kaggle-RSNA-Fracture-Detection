56846faa1f0f65c4938dc4c67f7003b586f5a11f
trantuan4132 <trantuan4132@gmail.com>
2022-09-22 11:42:15 +0700

    Add code to remove outlier for CSC and generate data for FD

--- utils/process_data.py
+++ utils/process_data.py
@@ -16,6 +16,8 @@ def parse_args():
                         help='Path to the label file')
     parser.add_argument('--relabel_path', type=str, default='relabel.csv',
                         help='Path to the relabel file')
+    parser.add_argument('--vert_label_path', type=str, default='train_CSC.csv',
+                        help='Path to the vertebrae label file')
     parser.add_argument('--seg_dir', type=str, default='segmentations',
                         help='Path to the segmentation directory')
     parser.add_argument('--out_dir', type=str, default='.',
@@ -26,6 +28,8 @@ def parse_args():
                         help='Extract patient id with their scan in reverse order')
     parser.add_argument('--get_vert_label', action='store_true',
                         help='Extract vertebrae labels from segmentation')
+    parser.add_argument('--get_frac_label', action='store_true',
+                        help='Create fracture labels')
     return parser.parse_args()
 
 
@@ -66,10 +70,31 @@ def load_nii(path):
 
 
 def extract_vertebrae_labels_from_segmentation(segmentation_dir='segmentations', image_dir='train_images', reverse_lst=[]):
+    """Extract vertebrae labels from segmentation"""
     label_cols = ['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7']
     
     def extract(path, image_dir='train_images', reverse_lst=[]):
-        seg_lst = []
+        def remove_outlier(seg_labels, direct='td'):
+            cnt = {}
+            start, step, end = (1, 1, 7) if direct=='td' else (7, -1, 1)
+            for idx in (range(len(seg)) if direct=='td' else range(len(seg)-1, -1, -1)):
+                new_seg_label = []
+                cd1 = start in seg_labels[idx]
+                cd2 = start+step in seg_labels[idx] and cnt.get(start, 0) > 0
+                if cd1 or cd2:
+                    if cd2 and not cd1:
+                        start += step
+                    s = start
+                    while True:
+                        cnt[s] = cnt.get(s, 0) + 1
+                        new_seg_label.append(s)
+                        s += step
+                        if s not in seg_labels[idx]:
+                            break
+                elif start == end:
+                    start += step
+                seg_labels[idx] = new_seg_label
+            return seg_labels
         
         # Load segmentation data
         seg = load_nii(path)
@@ -78,17 +103,26 @@ def extract_vertebrae_labels_from_segmentation(segmentation_dir='segmentations',
         uid = os.path.basename(path)[:-len('.nii')]
         slice_lst = sorted([int(f[:f.rfind('.')]) for f in os.listdir(f"{image_dir}/{uid}")], 
                            reverse=uid in reverse_lst)
-
+        
+        # Get list of labels each slice
+        seg_labels = []
+        for idx in range(len(seg)):
+            seg_labels.append(set(seg[idx].flatten()) & set(range(1, 8)))
+            
+        # Remove outliers
+        seg_labels = remove_outlier(seg_labels, direct='td')    # Top-down
+        seg_labels = remove_outlier(seg_labels, direct='bu')    # Bottom up
+            
         # Assign corresponding labels for each slice
+        seg_lst = []
         for idx, slice in enumerate(slice_lst):
-            seg_labels = set(seg[idx].flatten()) & set(range(1, 8))
             labels = np.zeros(7, dtype=int)
-            if len(seg_labels) > 0:
-                labels[np.array(list(seg_labels))-1] = 1
+            if len(seg_labels[idx]) > 0:
+                labels[np.array(list(seg_labels[idx]))-1] = 1
             seg_lst.append([uid, slice] + list(labels))
         return seg_lst
                 
-    seg_lst = Parallel(n_jobs=-1)(
+    seg_lst = Parallel(n_jobs=5)(
         delayed(extract)(path, image_dir, reverse_lst)
         for path in tqdm(glob.glob(f"{segmentation_dir}/*")) if path.endswith(".nii")
     )
@@ -99,9 +133,18 @@ def extract_vertebrae_labels_from_segmentation(segmentation_dir='segmentations',
     return seg_df
 
 
+def create_fracture_labels(df, df_vert, img_col, label_cols):
+    """Create fracture labels from train labels and vertebrae labels"""
+    vert_label_cols = [col + '_vert' for col in label_cols]
+    df_frac = df_vert.set_index(img_col).join(df.set_index(img_col), lsuffix='_vert').reset_index()
+    df_frac.loc[:, label_cols] = df_frac.loc[:, label_cols] * df_frac.loc[:, vert_label_cols]
+    return df_frac.drop(columns=vert_label_cols)
+
+
 if __name__ == "__main__":
     args = parse_args()
     img_col = 'StudyInstanceUID'
+    label_cols = ['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7']
     reverse_lst = None
 
     # Get list of scan in reverse order
@@ -134,4 +177,11 @@ if __name__ == "__main__":
         seg_df = extract_vertebrae_labels_from_segmentation(segmentation_dir=args.seg_dir, 
                                                             image_dir=args.image_dir, 
                                                             reverse_lst=reverse_lst)
-        seg_df.to_csv(f"{args.out_dir}/train_segmented.csv", index=False)
+        seg_df.to_csv(f"{args.out_dir}/train_CSC.csv", index=False)
+
+    # Create fracture labels
+    if args.get_frac_label:
+        df = pd.read_csv(args.label_path)
+        df_vert = pd.read_csv(args.vert_label_path)
+        df_frac = create_fracture_labels(df, df_vert, img_col, label_cols)
+        df_frac.to_csv(f"{args.out_dir}/train_FD.csv", index=False)
