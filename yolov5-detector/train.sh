c9212ac812a0e47d597408743c123ee50cf7ad13
Hung Le <hunglq.52@gmail.com>
2022-10-09 01:24:05 +0700

    Minor fix

--- yolov5-detector/train.sh
+++ yolov5-detector/train.sh
@@ -7,7 +7,7 @@ python data_preprocessing.py \
     --train_image_dir "/content/images_dir" \
     --metadata "/content/df_data.csv" \
     --bounding_boxes "/content/onedrive/Dataset/rsna-2022-cervical-spine-fracture-detection/train_bounding_boxes.csv" \
-    --yolo_dir "/content/drive/MyDrive/AIP490_Capstone_Project/kaggle-RSNA-Fracture-Detection/yolov5-detector/yolov5" 
+    --yolo_dir "/content/drive/MyDrive/FPT_Capstone_Project/kaggle-RSNA-Fracture-Detection/yolov5-detector/yolov5" 
 python train_hyperparams.py
 cd ./yolov5
-python classify/train.py --img 512 --batch 32 --epochs 80 --model "yolov5l-cls.pt" --data "/content/drive/MyDrive/AIP490_Capstone_Project/kaggle-RSNA-Fracture-Detection/yolov5-detector/yolov5/base_dir/images" 
+python classify/train.py --img 512 --batch 32 --epochs 80 --model "yolov5l-cls.pt" --data "/content/drive/MyDrive/FPT_Capstone_Project/kaggle-RSNA-Fracture-Detection/yolov5-detector/yolov5/base_dir/images" 
