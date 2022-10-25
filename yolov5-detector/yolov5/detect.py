7ba8652b5fb0cdd8f5d611c90a408b8fd8403eec
Hung Le <hunglq.52@gmail.com>
2022-10-25 10:59:40 +0700

    minor fix

--- yolov5-detector/yolov5/detect.py
+++ yolov5-detector/yolov5/detect.py
@@ -146,7 +146,7 @@ def run(
 
             p = Path(p)  # to Path
             save_path = str(save_dir / p.name)  # im.jpg
-            txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # im.txt
+            txt_path = str(save_dir / 'labels' / f'{p.parts[-2]}_{p.stem}') + ('' if dataset.mode == 'image' else f'_{frame}')  # im.txt
             s += '%gx%g ' % im.shape[2:]  # print string
             gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
             imc = im0.copy() if save_crop else im0  # for save_crop
