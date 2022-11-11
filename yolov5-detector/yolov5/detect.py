8b839ffb608899efbceff8ca71b7f66aea6dd963
Hung Le <hunglq.52@gmail.com>
2022-11-11 15:26:07 +0700

    minor fix

--- yolov5-detector/yolov5/detect.py
+++ yolov5-detector/yolov5/detect.py
@@ -47,6 +47,35 @@ from utils.general import (LOGGER, Profile, check_file, check_img_size, check_im
 from utils.plots import Annotator, colors, save_one_box
 from utils.torch_utils import select_device, smart_inference_mode
 
+def to_csv(predict_dir, save_dir, detect=False):
+    res = pd.DataFrame()
+    if not detect:
+        txt_files_list = os.listdir(predict_dir)
+        for i, txt_file in enumerate(txt_files_list):
+            # set the path
+            path = f'{predict_dir}/{txt_file}'
+            df = pd.read_csv(path, sep=" ", header=None)
+            id = [txt_file.split('_')[0]]
+            slc = [txt_file.split('_')[1].split('.')[0]]
+            fracture = [df.loc[df[1] == 'fracture', 0].iloc[-1]]
+            tmp = pd.DataFrame(data={'StudyInstanceUID': id, 'Slice': slc, 'fracture': fracture})
+            res = pd.concat([res, tmp], axis=0, ignore_index=True)
+    else:
+        txt_files_list = os.listdir(predict_dir)
+        for i, txt_file in enumerate(txt_files_list):
+            cols = ['class', 'x-center', 'y-center', 'bbox_width', 'bbox_height', 'conf-score']
+            path = f'{predict_dir}/{txt_file}'
+            df = pd.read_csv(path, sep=" ", header=None)
+            df.columns = cols
+            id = [txt_file.split('_')[0]]
+            slc = [txt_file.split('_')[1].split('.')[0]]
+            df['StudyInstanceUID'] = id
+            df['slice_num'] = slc
+            res = pd.concat([res, df], axis=0, ignore_index=True)
+            
+    
+    res.to_csv(save_dir, index=None)
+    return res
 
 @smart_inference_mode()
 def run(
@@ -167,6 +196,9 @@ def run(
                         line = (cls, *xywh, conf) if save_conf else (cls, *xywh)  # label format
                         with open(f'{txt_path}.txt', 'a') as f:
                             f.write(('%g ' * len(line)).rstrip() % line + '\n')
+                        res_path = f'{str(save_dir)}/labels/'
+                        save_path = f'{str(project)}/results.csv'
+                        to_csv(res_path, save_path, True)
 
                     if save_img or save_crop or view_img:  # Add bbox to image
                         c = int(cls)  # integer class
