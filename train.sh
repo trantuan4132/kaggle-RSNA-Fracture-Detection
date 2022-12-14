eb3ce52b390dbee8722890c2be76e3b052fb557b
trantuan4132 <trantuan4132@gmail.com>
2022-12-14 22:07:31 +0700

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,19 +1,22 @@
+INPUT_DIR="dataset"
+OUTPUT_DIR="output"
+
 python utils/process_data.py \
-    --image_dir "train_images" \
-    --label_path "train.csv" \
+    --image_dir "${INPUT_DIR}/train_images" \
+    --label_path "${INPUT_DIR}/train.csv" \
     --relabel_path "relabel.csv" \
-    --seg_dir "segmentations" \
-    --out_dir "dataset" \
+    --seg_dir "${INPUT_DIR}/segmentations" \
+    --out_dir $OUTPUT_DIR \
     --drop \
     --get_rvs_lst \
     --get_vert_label \
     --get_vert_bbox
 
 python utils/fold_split.py \
-    --label_path "train_vert_bbox_ratio.csv" \
+    --label_path "${OUTPUT_DIR}/train_vert_bbox_ratio.csv" \
     --multi_label \
     --kfold 5 \
-    --save_path "train_vert_bbox_ratio_fold5.csv"
+    --save_path "${OUTPUT_DIR}/train_vert_bbox_ratio_fold5.csv"
 
 # python utils/preprocess_checkpoint.py \
 #     --checkpoint_path "convnext_tiny_22k_1k_384.pth" \
@@ -28,18 +31,18 @@ done
 python inference.py --CFG config/CFG_vert_bbox_ratio_infer.yaml
 
 python utils/process_data.py \
-    --image_dir "train_images" \
-    --label_path "train.csv" \
-    --vert_label_path "infer_vert_bbox_ratio.pkl"
-    --out_dir "dataset" \
+    --image_dir "${INPUT_DIR}/train_images" \
+    --label_path "${INPUT_DIR}/train.csv" \
+    --vert_label_path "${OUTPUT_DIR}/infer_vert_bbox_ratio.pkl"
+    --out_dir $OUTPUT_DIR \
     --get_frac_label \
     --seq_len 24
 
 python fold_split.py \
-    --label_path 'vertebrae_df.pkl' \
-    --label_cols 'fractured' \
+    --label_path "${OUTPUT_DIR}/vertebrae_df.pkl" \
+    --label_cols "fractured" \
     --kfold 5 \
-    --save_path 'vertebrae_df_fold5.pkl'
+    --save_path "${OUTPUT_DIR}/vertebrae_df_fold5.pkl"
 
 for FOLD in 0 1 2 3 4
 do
